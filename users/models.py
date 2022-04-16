234dd0ebd5232486c0bd2cb0dbd61e2a2b4d9170
zzamy <hatank1013@gmail.com>
2022-04-16 09:48:22 +0530

    User model-3

--- users/models.py
+++ users/models.py
@@ -17,8 +17,30 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANG_KOR = "KOR"
+    LANG_ENG = "ENG"
+    LANG_CHOICE = (
+        (LANG_KOR, "KOREAN"),
+        (LANG_ENG, "ENGLISH"),
+    )
+
+    CURRENCY_USD = "USD"
+    CURRENCY_KRW = "KWR"
+    CURRENCY_CHOICE = (
+        (CURRENCY_KRW, "KOREAN WON"),
+        (CURRENCY_USD, "USD DOLLOR"),
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICE, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANG_CHOICE, null=True, max_length=10, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
